ddf0645f02de26f898755d146fa91e37f0173996
pawansharmazee5 <developer.pawan23@gmail.com>
2023-03-14 10:21:50 +0530

    InitialCommit
    InitialCommit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Pawan Sharma on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
